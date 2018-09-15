ff4af0d4a84c3d05470ff71996eada6a168fb55a
LeCray <mistercryptogrid@gmail.com>
2018-09-15 15:28:32 +0200

    Making the menu button for mobile!

--- src/Components/Landing/LandingMobile.js
+++ src/Components/Landing/LandingMobile.js
@@ -38,7 +38,8 @@ export default class LandingMobile extends Component {
             landingHome: false,
             about: false,
             work: false,
-            contact: false
+            contact: false,
+            menuOpen: false
             
         };
 
@@ -112,6 +113,10 @@ export default class LandingMobile extends Component {
         const workLink = workLink => this.workLink = workLink
         const contactLink = contactLink => this.contactLink = contactLink
 
+        const aboutLinkMenu = aboutLinkMenu => this.aboutLinkMenu = aboutLinkMenu
+        const workLinkMenu = workLinkMenu => this.workLinkMenu = workLinkMenu
+        const contactLinkMenu = contactLinkMenu => this.contactLinkMenu = contactLinkMenu
+
         const aboutHome = aboutHome => this.aboutHome = aboutHome
         const workHome = workHome => this.workHome = workHome
         const contactHome = contactHome => this.contactHome = contactHome
@@ -126,62 +131,83 @@ export default class LandingMobile extends Component {
                         <Dots size={25} animating={this.state.loading} color="#808080"/>
                     </div>                                                                                       
                 :
-                    
-                    <div ref={landingHome} className="landing-home" style={{fontFamily: "Josefin Sans", "-webkit-overflow-scrolling": "touch"}}>
-                        <Row>
-                            
-                            <Col lg={12} className="top-section">
-                                <img ref={rocket} className="rocket" src={require("../../Assets/Images/thunder.svg")}/><br/>
+                    <div>
+                        <img 
+                            className="menu" 
+                            src={require("../../Assets/Images/menu-white.svg")}
+                            onClick={() => this.setState({menuOpen: !this.state.menuOpen})}
+                        />
+                        <img className="menu-hexagon" src={require("../../Assets/Images/menu-hexagon.svg")}/>
+
+                        <div className="menu-mobile" style={{display: this.state.menuOpen?null:"none"}}>
+                            <p ref={aboutLinkMenu} className="link-mobile-menu">                                    
+                                About 
+                            </p>
+                            <p ref={workLinkMenu} className="link-mobile-menu">
+                                Work 
+                            </p>
+                            <p ref={contactLinkMenu} className="link-mobile-menu">
+                                Contact 
+                            </p>
+                        </div>
+
+                        <div ref={landingHome} className="landing-home" style={{fontFamily: "Josefin Sans", "-webkit-overflow-scrolling": "touch"}}>                                      
+
+                            <Row>
                                 
-                                <h1 ref={Fname} className="name">Jabulani</h1>
-                                <h1 ref={Lname} className="name" style={{marginLeft: 5}}>Kunene</h1>
-                                <hr ref={hr} className={this.state.mobile?"hr-mobile":"hr"}/>
+                                <Col lg={12} className="top-section">
+                                    <img ref={rocket} className="rocket" src={require("../../Assets/Images/thunder.svg")}/><br/>
+                                    
+                                    <h1 ref={Fname} className="name">Jabulani</h1>
+                                    <h1 ref={Lname} className="name" style={{marginLeft: 5}}>Kunene</h1>
+                                    <hr ref={hr} className={this.state.mobile?"hr-mobile":"hr"}/>
 
 
-                                <div ref={summary} >                                    
-                                    <h6 className="summary-mobile">Full Stack Developer</h6>
-                                    {/*<h6 className="summary-mobile">ReactJS, ExpressJS, Ruby on Rails</h6>*/}
-                                    <h6 className="summary-mobile">Web | Android | iOS</h6>                                    
-                                </div>
+                                    <div ref={summary} >                                    
+                                        <h6 className="summary-mobile">Full Stack Developer</h6>
+                                        {/*<h6 className="summary-mobile">ReactJS, ExpressJS, Ruby on Rails</h6>*/}
+                                        <h6 className="summary-mobile">Web | Android | iOS</h6>                                    
+                                    </div>
 
-                                
-                                <div ref={whiteBox} className="white-box">
-                                    <img ref={me} className="me-mobile" src={require("../../Assets/Images/me3.jpg")}/>
-                                </div>
-                                
-                                
-                                
+                                    
+                                    <div ref={whiteBox} className="white-box">
+                                        <img ref={me} className="me-mobile" src={require("../../Assets/Images/me3.jpg")}/>
+                                    </div>
+                                    
+                                    
+                                    
 
-                                <div className="links-mobile">
-                                    <p ref={aboutLink} className="link-mobile" onClick={()=>scrollToComponent(this.about,{offset:0,align:'top',duration:1500})}>                                    
-                                        About /
-                                    </p>
-                                    <p ref={workLink} className="link-mobile" onClick={()=>scrollToComponent(this.work,{offset:0,align:'top',duration:1500})}>
-                                        Work /
-                                    </p>
-                                    <p ref={contactLink} className="link-mobile" onClick={()=>scrollToComponent(this.contact,{offset:0,align:'top',duration:1500})}>
-                                        Contact /
-                                    </p>
-                                </div>
+                                    <div className="links-mobile">
+                                        <p ref={aboutLink} className="link-mobile" onClick={()=>scrollToComponent(this.about,{offset:0,align:'top',duration:1500})}>                                    
+                                            About /
+                                        </p>
+                                        <p ref={workLink} className="link-mobile" onClick={()=>scrollToComponent(this.work,{offset:0,align:'top',duration:1500})}>
+                                            Work /
+                                        </p>
+                                        <p ref={contactLink} className="link-mobile" onClick={()=>scrollToComponent(this.contact,{offset:0,align:'top',duration:1500})}>
+                                            Contact /
+                                        </p>
+                                    </div>
 
-                                
-                            </Col>                                                                             
-                        </Row>     
-                        <div ref={(section) => { this.about = section; }} className="about-home">                 
-                            <AboutMobile />
-                        </div>                
+                                    
+                                </Col>                                                                             
+                            </Row>     
+                            <div ref={(section) => { this.about = section; }} className="about-home">                 
+                                <AboutMobile />
+                            </div>                
 
-                    
-                        <div ref={(section) => { this.work = section; }} className="work-home">                 
-                            <WorkMobile />
-                        </div>                
-
-                    
-                        <div ref={(section) => { this.contact = section; }} className="contact-home">                 
-                            <ContactMobile />
-                        </div>                
                         
-                    </div>  
+                            <div ref={(section) => { this.work = section; }} className="work-home">                 
+                                <WorkMobile />
+                            </div>                
+
+                        
+                            <div ref={(section) => { this.contact = section; }} className="contact-home">                 
+                                <ContactMobile />
+                            </div>                
+                            
+                        </div>  
+                    </div>
                     
                 }
 

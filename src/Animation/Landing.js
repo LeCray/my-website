806cc0a3f2c267fbc7d503ba80e63774b6d3bd1c
LeCray <mistercryptogrid@gmail.com>
2018-09-22 00:31:40 +0200

    Landing page is fixed!

--- src/Animation/Landing.js
+++ src/Animation/Landing.js
@@ -28,20 +28,24 @@ export const topSection = (landingHome,rocket,Fname,Lname, whiteBox, me, hr, sum
         y: 10,       
         ease: Elastic.easeOut.config(.9, .8),
     }, "start+=1.2")
-    .from(Fname, 4, {
+    /*.from(Fname, 4, {
         opacity: 0, 
         x:-100, 
         y:5,
         ease: Back.easeInOut.config(2),
         delay: -.5
-    }, "start+=.25")
+    }, "start+=.35")
     .from(Lname, 3.8, {
         opacity: 0, 
-        x:-200, 
+        x:-100, 
         y:5,
-        ease: Back.easeInOut.config(1),
+        ease: Back.easeInOut.config(2),
         delay: -.5
-    }, "start+=.25")
+    }, "start+=.35")*/
+
+    .staggerFrom([Lname,Fname], 5, {
+        x:-100,y:10, opacity:0, ease: Elastic.easeOut.config(1.2, .9)
+    }, .2, "start+=1.4")
 
     .addLabel("firstPartIsDone") //FIRST PART DONE
 
@@ -60,31 +64,32 @@ export const topSection = (landingHome,rocket,Fname,Lname, whiteBox, me, hr, sum
 
     .to(rocket, 2.3, {  
     	opacity: 0,             
-        x:300,         
+        x:400,         
         ease: Back.easeInOut.config(2),
         delay: 0
-    }, "firstPartIsDone-=1.9")
+    }, "firstPartIsDone-=3")
 
     .addLabel("rocketInPlace")
     .to(Fname, 1, {      	
         x:150, 
         ease: Power4.easeInOut,        
-    }, "firstPartIsDone-=1.02")
+    }, "firstPartIsDone-=2.12")
     .to(Lname, 1, {            	
         x:155, 
         ease: Power4.easeInOut,        
-    }, "firstPartIsDone-=1.1") 
+    }, "firstPartIsDone-=2.2") 
     .to(Fname, 2, {      	
         y:-100, 
         ease: Power2.easeOut,
         opacity: 1
-    }, "firstPartIsDone-=.5")
+    }, "firstPartIsDone-=1.7")
     .to(Lname, 2, {            	
         y:-100, 
         ease: Power2.easeOut,
         delay: .5,
         opacity: 1
-    }, "firstPartIsDone-=1") 
+    }, "firstPartIsDone-=2.2") 
+
     .to(hr, 2, {
         opacity: 1,
         ease: Power2.easeOut,
